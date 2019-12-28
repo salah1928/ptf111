914ee2d0b9bacddc8ec76800a5a4e9678d81deea
Tizmo1928 <tizmo1928@gmail.com>
2019-12-28 19:39:38 +0100

    ijfjzf

--- assets/javascript/phone.js
+++ assets/javascript/phone.js
@@ -1,7 +1,9 @@
 const ps = document.querySelectorAll('.fader')
 const hamburger = document.getElementById("ham")
 const navl =  document.querySelectorAll('.navL')
-
+const down = document.getElementById('down')
+const footer = document.getElementById('footerHide')
+const cpr = document.getElementById('cpr')
 const appearopts = {
     threshold: 1,
     rootMargin:"0px",
@@ -28,4 +30,10 @@ function showNav(){
     navl.forEach(l=>{
         l.classList.toggle('show')
     })
+}
+function showFoot(){
+    footer.classList.toggle('hidden1')
+    cpr.classList.toggle('hidden')
+    down.classList.toggle('turned')
+    window.scrollTo(0,document.body.scrollHeight);
 }
